e5895ae17984aaecde4323090549b6bfb98d9dc5
Ahsan Khan <asifuddinahmed123@gmail.com>
2021-10-12 01:42:47 +0600

    Update configs

--- src/Server.java
+++ src/Server.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class Server extends Thread {
+public class Server {
     public static void main(String[] args) {
         HashMap<String, User> data = new HashMap<>();
 
